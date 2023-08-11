e422ed193ede8848e1fb71840f0ba200ff53bde7
Heber Marcano <Hebelioo@gmail.com>
2023-08-11 13:03:07 -0400

    added resolvers and typedefs

---
+++ server/schemas/typedef.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    posts: [Post]
+  }
+
+  type Post {
+    _id: ID
+    title: String
+    description: String
+    createdAt: String
+    tag: String
+    requester_id: User
+    interested_users: [User]
+    budget: Int
+  }
+
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
